5a135f1beb70b360a04b35e96f38360c9ba96de6
Evan Tahler <evantahler@gmail.com>
2015-08-27 14:57:25 +0100

    Update source.sql

--- books/etl/source.sql
+++ books/etl/source.sql
@@ -1,16 +1,3 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Version 4096
-#
-# http://www.sequelpro.com/
-# http://code.google.com/p/sequel-pro/
-#
-# Host: 127.0.0.1 (MySQL 5.6.26)
-# Database: source
-# Generation Time: 2015-08-27 13:46:24 +0000
-# ************************************************************
-
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
